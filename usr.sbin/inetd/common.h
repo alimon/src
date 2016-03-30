9979c103f7dda480c36aaed52d0e75e7f35c9b11
Anibal Limon <limon.anibal@gmail.com>
2016-03-29 19:19:59 -0600

    usr.sbin/inetd: Move struct servtab and struct biltin to common.h.
    Signed-off-by: Anibal Limon <limon.anibal@gmail.com>

--- usr.sbin/inetd/common.h
+++ usr.sbin/inetd/common.h
@@ -1,7 +1,64 @@
 #ifndef _INETD_COMMON_H_
 #define _INETD_COMMON_H_
 
+#include <sys/time.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet6/in6.h>
+#include <sys/un.h>
+
 #define	A_CNT(a)	(sizeof (a) / sizeof (a[0]))
+
+struct	servtab {
+	char	*se_hostaddr;		/* host address to listen on */
+	char	*se_service;		/* name of service */
+	int	se_socktype;		/* type of socket to use */
+	int	se_family;		/* address family */
+	char	*se_proto;		/* protocol used */
+	int	se_sndbuf;		/* sndbuf size */
+	int	se_rcvbuf;		/* rcvbuf size */
+	int	se_rpcprog;		/* rpc program number */
+	int	se_rpcversl;		/* rpc program lowest version */
+	int	se_rpcversh;		/* rpc program highest version */
+#define isrpcservice(sep)	((sep)->se_rpcversl != 0)
+	pid_t	se_wait;		/* single threaded server */
+	short	se_checked;		/* looked at during merge */
+	char	*se_user;		/* user name to run as */
+	char	*se_group;		/* group name to run as */
+	struct	biltin *se_bi;		/* if built-in, description */
+	char	*se_server;		/* server program */
+#define	MAXARGV 64
+	char	*se_argv[MAXARGV+1];	/* program arguments */
+#ifdef IPSEC
+	char	*se_policy;		/* IPsec poilcy string */
+#endif
+	struct accept_filter_arg se_accf; /* accept filter for stream service */
+	int	se_fd;			/* open descriptor */
+	int	se_type;		/* type */
+	union {
+		struct	sockaddr se_un_ctrladdr;
+		struct	sockaddr_in se_un_ctrladdr_in;
+		struct	sockaddr_in6 se_un_ctrladdr_in6;
+		struct	sockaddr_un se_un_ctrladdr_un;
+	} se_un;			/* bound address */
+#define se_ctrladdr	se_un.se_un_ctrladdr
+#define se_ctrladdr_in	se_un.se_un_ctrladdr_in
+#define se_ctrladdr_un	se_un.se_un_ctrladdr_un
+	int	se_ctrladdr_size;
+	int	se_max;			/* max # of instances of this service */
+	int	se_count;		/* number started since se_time */
+	struct	timeval se_time;	/* start of se_count */
+	struct	servtab *se_next;
+} *servtab;
+
+struct biltin {
+	const char *bi_service;		/* internally provided service name */
+	int	bi_socktype;		/* type of socket supported */
+	short	bi_fork;		/* 1 if should fork before call */
+	short	bi_wait;		/* 1 if should wait for child */
+	void	(*bi_fn)(int, struct servtab *);
+};
 
 static int my_signals[] =
     { SIGALRM, SIGHUP, SIGCHLD, SIGTERM, SIGINT, SIGPIPE };
